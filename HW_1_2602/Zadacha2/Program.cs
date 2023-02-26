eb858e91a178349fd0248b204dde41926d5d1b96
Maria Pita <mahoni@bk.ru>
2023-02-27 01:10:42 +0300

    Zadacha_2

---
+++ HW_1_2602/Zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("a = ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("c = ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
+
