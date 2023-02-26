e5b8f16ddacd71108ea473d2c24a9b5d9797e5b9
Maria Pita <mahoni@bk.ru>
2023-02-26 15:13:53 +0300

    Task 1_2

---
+++ Urok_1_2302/1_2/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("введите ваше число");
+int a = int.Parse(Console.ReadLine()!);
+if(a == 1)
+{
+    Console.WriteLine("Montag");
+}
+else if(a == 2)
+{
+    Console.WriteLine("Dinstag");
+}
+
+else if(a == 3)
+{
+    Console.WriteLine("Mitwoch");
+}
+
+else if(a == 4)
+{
+    Console.WriteLine("Donerstag");
+}
+
+else if(a == 5)
+{
+    Console.WriteLine("Freitag");
+}
+
+else if(a == 6)
+{
+    Console.WriteLine("Sondabend");
+}
+
+else if(a == 7)
+{
+    Console.WriteLine("Sontag");
+}
+
+else
+{
+    Console.WriteLine("false");
+}
