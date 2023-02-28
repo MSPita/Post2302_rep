fb7109674a650eebe176a45b58cd491772997374
Maria Pita <mahoni@bk.ru>
2023-02-28 22:40:56 +0300

    2_1, 2_1_1

--- Lesson_2/2_1_1/Program.cs
+++ Lesson_2/2_1_1/Program.cs
@@ -1,9 +1,16 @@
 ﻿//Напишите программу, которая принимает на вход трехзначное число 
 // и на выходе показывает последнюю цифру этого числа.
 
-// Вариант 1. Простой!
+// Вариант 2. Функция (примороченный).
 
-int num = new Random().Next(100, 1000);
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
 
-Console.WriteLine(num);
-Console.WriteLine(num % 10);
+}
+
+int result = TakeNum (new Random().Next(100, 1000));
+
+Console.WriteLine(result);
+
---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// // 3. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int N = int.Parse(Console.ReadLine()!);
+
+void Div(int N)
+{
+    if (N % 7 == 0 & N % 23 == 0)
+        Console.WriteLine("Кратно");
+
+    else
+        Console.WriteLine("Некратно");
+
+}
+//Console.WriteLine("Hello, World!");
+Div(N);
