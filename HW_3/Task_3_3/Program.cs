1c74236cccf13b167bbfd1ebbfe6bcfe7939d5d7
Maria Pita <mahoni@bk.ru>
2023-03-03 23:57:03 +0300

    Task 3_3

---
+++ HW_3/Task_3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//напишите программу, которая принимает на вход число N
+// и выдает таблицу кубов чисел от 1 до N.
+
+int num = int.Parse(Console.ReadLine()!);
+
+void Div(int n)
+{
+    int i = 1; 
+    while(i <= n)
+    {
+      Console.WriteLine(Math.Pow(i, 3));
+      i++;
+
+    }
+}
+Div(num);
+
