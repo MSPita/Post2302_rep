cefc1c333c4d786116204967a9c84146054a462c
Maria Pita <mahoni@bk.ru>
2023-03-03 14:09:00 +0300

    Task 2_1

---
+++ HW_2/Task_hw_2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+void Div(int a)
+{
+    int result = a / 10 % 10;
+    Console.WriteLine(result);
+
+}
+Div(num);
