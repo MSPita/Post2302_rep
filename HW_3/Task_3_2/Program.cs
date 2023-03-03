7d298c82957f5229fb9274ab8e51685797c4d744
Maria Pita <mahoni@bk.ru>
2023-03-03 23:21:48 +0300

    Task 3_2

---
+++ HW_3/Task_3_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напищите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3Д пространстве.
+
+double Dis(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2));
+
+}
+Console.WriteLine(Math.Round(Dis(3, 6, 8, 2, 1, -7),2, MidpointRounding.AwayFromZero));
