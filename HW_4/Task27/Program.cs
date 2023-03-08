c19efc89e1b65f9aebbb088f63cb6e4c204562f9
Maria Pita <mahoni@bk.ru>
2023-03-09 00:27:05 +0300

    Task27

---
+++ HW_4/Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число -> ");
+int A = int.Parse(Console.ReadLine()!);
+
+
+void SumNum(int num)
+{
+    int sum = 0;
+    for(int i = 0; num > 0; i++)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+
+    }
+        
+
+}
+
+
+SumNum(A);
+
+//Console.WriteLine();
+
+//Console.Write("");
+//Console.Write(result);
+
+//Console.WriteLine("Hello, World!");
