cdedb8a5ac57387a41d5568cdeccbd0bf25a7861
Maria Pita <mahoni@bk.ru>
2023-03-10 23:25:01 +0300

    Task29 v.2

--- HW_4/Task29/Program.cs
+++ HW_4/Task29/Program.cs
@@ -1,17 +1,35 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Оформите вывод и заполнение в виде функции.
 
-void massive(int mas)
+void Print(int[] arr)
 {
-    int[] numbers = new int[mas];
-    for (int i = 0; i < mas; i++)
-    {
-        numbers[i] = new Random().Next(100);
-        Console.WriteLine(numbers[i]);
+    int size = arr.Length;
 
-    }
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
 }
-Console.WriteLine("Задайте длину массива ");
-int length = int.Parse(Console.ReadLine()!);
 
-massive(length);
+
+int[] Mass(int size, int from, int to)
+{
+    int[] numbers = new int[size];
+
+    for (int i = 0; i < size; i++)
+        numbers[i] = new Random().Next(from, to);
+    return numbers;    
+    
+}
+
+Console.WriteLine("Задайте размер массива ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Задайте начало массива ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Задайте конец массива ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[]mass = Mass(num, start, stop);
+Print(mass);
+
 
