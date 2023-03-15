9fe2ccc47467f55844d88369e78ef115f0099cd9
Maria Pita <mahoni@bk.ru>
2023-03-15 16:46:20 +0300

    L6 6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// напишите программу, которая принимапет на вход 
+//три числа и проверяет, может ли сущетвовать треугольник 
+// со сторонами такой длины. (Длина стороны меньше суммы длин 2 других)
+
+void Triangle(int A, int B, int C)
+{
+    if (A < B + C & B < A + C & C < A + B)
+        Console.WriteLine("Yes");
+
+    else
+        Console.WriteLine("No");
+
+}
+int N1 = int.Parse(Console.ReadLine()!);
+int N2 = int.Parse(Console.ReadLine()!);
+int N3 = int.Parse(Console.ReadLine()!);
+
+Triangle(N1, N2, N3);
