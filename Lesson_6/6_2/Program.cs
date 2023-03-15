38a2d0f18b8490da1570a9ff0ee36eb26779bed6
Maria Pita <mahoni@bk.ru>
2023-03-15 17:06:37 +0300

    L6 6_2

---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая будет преобразовывать 
+//десятичное число в двоичное.
+
+string ConvertBin(int num)
+
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+
+    return result;
+}
+int n = int.Parse(Console.ReadLine()!);
+string ResulEnd = ConvertBin(n);
+Console.WriteLine(ResulEnd);
