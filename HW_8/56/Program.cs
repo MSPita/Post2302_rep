56523d084997e090853fe0a87cff067e9da1e8f4
Maria Pita <mahoni@bk.ru>
2023-03-23 18:41:05 +0300

    56

--- HW_8/56/Program.cs
+++ HW_8/56/Program.cs
@@ -9,7 +9,7 @@ void Print(int[,] arr)
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < column; j++)
-            Console.Write($" {arr[i, j]} ,3");
+            Console.Write($" {arr[i, j],3}");
         Console.WriteLine();
     }
     Console.WriteLine();
@@ -23,4 +23,47 @@ int[,] Massiv(int row, int column, int from, int to)
         for (int j = 0; j < column; j++)
             arr[i, j] = new Random().Next(from, to);
     return arr;
+
 }
+
+
+int[] SumRows(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int[] sum = new int[row];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            sum[i] += arr[i, j];
+    return sum;
+}
+
+int Min(int[] arr)
+{
+    int minindex = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] < arr[minindex]) minindex = i;
+    return minindex;
+}
+
+
+Console.Write("Количество строк -> ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Количество столбцов -> ");
+int column_num = int.Parse(Console.ReadLine()!);
+Console.Write("Начало диапазона -> ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Конец диапазона -> ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] mass = Massiv(row_num, column_num, start, stop);
+Print(mass);
+
+int[] sums = SumRows(mass);
+int numRow = Min(sums);
+Console.WriteLine($"номер строки с наименьшей суммой {numRow+1}");
+
+
+
