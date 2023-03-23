dbe851647c3ec4bec677077af36acd6737fed9d1
Maria Pita <mahoni@bk.ru>
2023-03-23 12:34:56 +0300

    L9 all task

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Рекурсия
+//https://www.youtube.com/watch?v=F4HNO4rCQ2c&ab_channel=KhanAcademyRussian
+//https://youtu.be/rh1mP02NFoM?t=229
+//https://skillbox.ru/media/code/rekursiya-vokrug-nas/
+//
+//Задайте занчение N. Напишите программу, которая
+// выведет все натуральные числав промежутке
+// от 1 до N при помощи рекурсии.
+
+void SeriesNums(int N)
+{
+
+    if (N == 0) return;
+    SeriesNums(N - 1);
+    Console.Write($"{N}, ");
+
+}
+
+SeriesNums(int.Parse(Console.ReadLine()!));
